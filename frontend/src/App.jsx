f07de784a2f59494ec5f6a76971e4331b0950ca1
W45 <73011833+wgs45@users.noreply.github.com>
2025-04-18 21:37:13 +0800

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "./services/api";
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((res) => setProducts(res.data));
+  }, []);
+
   return (
-    <>
-      <div className="app">
-        <h1>Hello world</h1>
-      </div>
-    </>
+    <div>
+      <h1>Product list</h1>
+      {products.map((p) => (
+        <div key={p.id}>
+          <h2>{p.name}</h2>
+          <p>{p.description}</p>
+          <span>${p.price}</span>
+        </div>
+      ))}
+    </div>
   );
 }
 
