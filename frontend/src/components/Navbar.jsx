bada945606d6f7ad82c526b0c74c670e77ec71be
W45 <warrengs51@gmail.com>
2025-05-08 21:21:44 +0800

    [Edit] Navbar.jsx
    UI/UX improvement

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,36 +1,97 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { AppBar, Toolbar, Button } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Menu,
+  MenuItem,
+  Typography,
+  Box,
+  IconButton,
+} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Navbar() {
   const { user, logout } = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/">
+    <AppBar position="static" color="primary" elevation={3}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          color="inherit"
+          sx={{ textDecoration: "none" }}
+        >
+          Online Ordering System
+        </Typography>
+
+        {/* Left: Home */}
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          color="inherit"
+          sx={{ textDecoration: "none" }}
+        >
           Home
-        </Button>
-        {user ? (
-          <>
-            <Button color="inherit" component={Link} to="/dashboard">
-              Dashboard ({user?.name?.split(" ")[0]})
-            </Button>
-            <Button color="inherit" onClick={logout}>
-              Logout
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={Link} to="/register">
-              Register
-            </Button>
-          </>
-        )}
+        </Typography>
+
+        {/* Right: Auth Options */}
+        <Box>
+          {user ? (
+            <>
+              <Button
+                color="inherit"
+                onClick={handleMenuOpen}
+                endIcon={<ArrowDropDownIcon />}
+              >
+                {user.name?.split(" ")[0]}
+              </Button>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem
+                  onClick={handleMenuClose}
+                  component={Link}
+                  to="/dashboard"
+                >
+                  Dashboard
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleMenuClose();
+                    logout();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/register">
+                Register
+              </Button>
+            </>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
